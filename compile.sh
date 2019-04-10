a8b81c47add6a6104932fce9002d06d91df0698d
jujucom@126.com <jujucom@126.com>
2019-04-10 22:42:03 +0800

    update

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sass --style expanded wui-mini.scss dist/wui-mini.css --trace
-sass --style compressed wui-mini.scss dist/wui-mini-min.css
+node_modules/node-sass/bin/node-sass --style expanded wui-mini.scss dist/wui-mini.css --trace
+node_modules/node-sass/bin/node-sass --style compressed wui-mini.scss dist/wui-mini-min.css
 
-sass --style expanded wui-bundle.scss dist/wui-bundle.css --trace
-sass --style compressed wui-bundle.scss dist/wui-bundle-min.css
+node_modules/node-sass/bin/node-sass --style expanded wui-bundle.scss dist/wui-bundle.css --trace
+node_modules/node-sass/bin/node-sass --style compressed wui-bundle.scss dist/wui-bundle-min.css
 
 rm -f dist/*.map
